33ed6dc7eadb116b271b47317d03d6ef41bb8787
Filipe Santos <filipemds@hotmail.com>
2019-05-16 09:46:28 -0300

    first step

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
 import 'react-perfect-scrollbar/dist/css/styles.css';
+import '@viridis/ui/assets/css/antd.css';
+import '@viridis/ui/assets/css/icon-font.css';
+import '@viridis/ui/assets/css/main.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
